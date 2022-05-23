6469b704355bafc58902cbfea4bd384d743f2c94
forgetechy <79404263+forgetechy@users.noreply.github.com>
2022-05-23 16:14:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var modal = document.getElementById('myModal');
+
+
+var img = $('.myImg');
+var modalImg = $("#img01");
+var captionText = document.getElementById("caption");
+$('.myImg').click(function(){
+    modal.style.display = "block";
+    var newSrc = this.src;
+    modalImg.attr('src', newSrc);
+    captionText.innerHTML = this.alt;
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
